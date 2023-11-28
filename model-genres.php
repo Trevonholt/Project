32ecf7d0c6c0439eabdf2f902114b6e57b7a443b
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-28 12:34:53 -0600

    Create model-genres.php

---
+++ model-genres.php
@@ -0,0 +1,15 @@
+<?php
+function selectGenres() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT genre_id, genre_type FROM `genre`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
