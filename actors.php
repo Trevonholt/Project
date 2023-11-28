ed9c2c1d7aa070c15c44e2973224d944e5f3beb0
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-28 14:59:22 -0600

    Update actors.php

--- actors.php
+++ actors.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-actors.php");
 
+$pageTitle="genres";
+include "view-header.php";
+$actors=selectActors();
+include "view-actors.php";
+include "view-footer.php";
+?>
