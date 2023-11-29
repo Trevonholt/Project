b2ddd95059071f6c7850a50c77e3d6df674c7b40
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 12:50:09 -0600

    Update view-directors.php

--- view-directors.php
+++ view-directors.php
@@ -31,6 +31,11 @@ while ($director=$directors->fetch_assoc()){
     <td><?php echo $director['director_lname'];?></td>
     <td><?php echo $director['director_number'];?></td>
     <td>
+<?php
+include "view-directors-editform.php";
+?>
+    </td>
+    <td>
       <form method="post" action="">
         <input type="hidden" name="did" value="<?php echo $director['director_id'];?>">
         <input type="hidden" name="actionType" value="Delete">
@@ -40,9 +45,6 @@ while ($director=$directors->fetch_assoc()){
             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
           </svg>
         </button>
-    </td>
-    <td>
-      
     </td>
   </tr>
   <?php
