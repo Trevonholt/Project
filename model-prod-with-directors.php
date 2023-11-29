0a980f5e4e7cc56d8617766987c9ca5d4b67885c
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 11:03:46 -0600

    Update model-prod-with-directors.php

--- model-prod-with-directors.php
+++ model-prod-with-directors.php
@@ -12,4 +12,18 @@ function selectProds() {
         throw $e;
     }
 }
+function selectDirectorsByProd($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT d.director_id,director_fname,director_lname,director_number,prod_name FROM `directors` d JOIN movies m on m.director_id=d.director_id JOIN prodComp p on m.prod_id=p.prod_id WHERE m.prod_id=?");
+        $stmt->bind_param("i", $did);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
