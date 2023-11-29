d30f9c78f4fb823691ad75846ee3b4a302dcd7d5
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 12:23:30 -0600

    Update directors.php

--- directors.php
+++ directors.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch($_POST['actionType']){
     case "add":
-      insertDirectors($_POST['dFname'], $_POST['dLname'],$_POST['dNum']);
+      if (insertDirectors($_POST['dFname'], $_POST['dLname'],$_POST['dNum'])){
+          echo '<div class="alert alert-success" role="alert">Director Added.</div>"';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>"';
+        }
       break;
   }
 }
