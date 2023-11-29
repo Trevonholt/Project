9fbc644092928af06be18d21224fe7fba8c7dab6
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 12:24:47 -0600

    Update directors.php

--- directors.php
+++ directors.php
@@ -9,9 +9,9 @@ if (isset($_POST['actionType'])){
   switch($_POST['actionType']){
     case "add":
       if (insertDirectors($_POST['dFname'], $_POST['dLname'],$_POST['dNum'])){
-          echo '<div class="alert alert-success" role="alert">Director Added.</div>"';
+          echo '<div class="alert alert-success" role="alert">Director Added.</div>';
         } else{
-          echo '<div class="alert alert-danger" role="alert">Error</div>"';
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
         }
       break;
   }
