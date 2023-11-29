9a12f70ac4d83896a72d8b1116d6807f1dabb81f
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 11:22:19 -0600

    Update view-prod-with-instructors.php

--- view-prod-with-instructors.php
+++ view-prod-with-instructors.php
@@ -7,14 +7,16 @@ while ($prod=$prods->fetch_assoc()){
     <div class="card-body">
       <h5 class="card-title"><?php echo $prod['prod_name'];?></h5>
       <p class="card-text">
+  <ul class="list-group">
 <?php
   $directors=selectDirectorsByProd($prod['prod_id']);
   while ($director=$directors->fetch_assoc()){
 ?>
-    
+    <li class="list-group-item"><?php echo $producer['director_fname'];?> - <?php echo $producer['director_lname'];?> - <?php echo $producer['director_number'];?></li>
 <?php
   }
 ?>
+  </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">State: <?php echo $prod['state'];?></small></p>
     </div>
