ae99e3674c1120b58e7fa7e66d07a45fb09f058e
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 12:37:25 -0600

    Update directors.php

--- directors.php
+++ directors.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])){
           echo '<div class="alert alert-danger" role="alert">Error</div>';
         }
       break;
+    case "Delete":
+      if (insertDirectors($_POST['did'])){
+          echo '<div class="alert alert-success" role="alert">Director Deleted.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
   }
 }
 $directors=selectDirectors();
