f0f25304c9a689264c79da5fec5cf53df6243c2a
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 12:14:52 -0600

    Update directors.php

--- directors.php
+++ directors.php
@@ -4,6 +4,14 @@ require_once("model-directors.php");
 
 $pageTitle="genres";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch($_POST['actionType'])){
+  case "add":
+    insertDirector($_POST['dFname'], $_POST['dLname'],$_POST['dNum']);
+    break;
+  }
+}
 $directors=selectDirectors();
 include "view-directors.php";
 include "view-footer.php";
