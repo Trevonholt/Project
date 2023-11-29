4b1bd4ff0f28b0bc6d518122b927bcb8aa8df3e9
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 12:59:58 -0600

    Update directors.php

--- directors.php
+++ directors.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])){
           echo '<div class="alert alert-danger" role="alert">Error</div>';
         }
       break;
+    case "Edit":
+      if (updateDirectors($_POST['dFname'], $_POST['dLname'],$_POST['dNum'],$_POST['did'])){
+          echo '<div class="alert alert-success" role="alert">Director Edited.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
     case "Delete":
       if (deleteDirectors($_POST['did'])){
           echo '<div class="alert alert-success" role="alert">Director Deleted.</div>';
