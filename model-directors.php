21d1ee5f72a9a66825253a549d26921688f84878
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-29 11:44:08 -0600

    Update model-directors.php

--- model-directors.php
+++ model-directors.php
@@ -12,4 +12,46 @@ function selectDirectors() {
         throw $e;
     }
 }
+
+function insertDirectors($dFname, $dLname, $dNum) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `directors` (`director_fname`, `director_lname`, `director_number`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $dFname, $dLname, $dNum);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateDirectors($dFname, $dLname, $dNum, $did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `directors` set `director_fname`=?, `director_lname`=?, `director_number`=? where director_id=?");
+        $stmt->bind_param("sssi", $dFname, $dLname, $dNum, $did);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteDirectors($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from directors where director_id=?");
+        $stmt->bind_param("i", $did);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
