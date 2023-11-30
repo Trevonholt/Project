e93247b4e61da9e089bf3917f899896ee1d0db43
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 15:08:59 -0600

    Update view-genres-editform.php

--- view-genres-editform.php
+++ view-genres-editform.php
@@ -1,9 +1,9 @@
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editGenreModal<?php echo $genre['genre_id'];?>">
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-</svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+  </svg>
 </button>
 
 <!-- Modal -->
@@ -18,10 +18,10 @@
         <form method="post" action="">
           <div class="mb-3">
             <label for="gType<?php echo $genre['genre_id'];?>" class="form-label">Genre Type</label>
-            <input type="text" class="form-control" id="gType<?php echo $genre['genre_id'];?>" name="gType" value="<?php echo $genre['genre_type'];?>">
+            <input type="text" class="form-control" id="gType" name="gType" value="<?php echo $genre['genre_type'];?>">
           </div>
-            <input type="hidden" name="gid" value="gType<?php echo $genre['genre_id'];?>">
-            <input type="hidden" name="actionType" value="edit">
+             <input type="hidden" name="gid" value="<?php echo $genre['genre_id'];?>">
+            <input type="hidden" name="actionType" value="add">
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
