ea43275baf5fa1e38eb131f4a71dfa0122fdb4bb
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 15:53:25 -0600

    Update prod.php

--- prod.php
+++ prod.php
@@ -4,6 +4,33 @@ require_once("model-prods.php");
 
 $pageTitle="Production Companies";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch($_POST['actionType']){
+    case "add":
+      if (insertProds($_POST['pName'], $_POST['state'],$_POST['city'])){
+          echo '<div class="alert alert-success" role="alert">Production Company Added.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
+    case "edit":
+      if (updateProds($_POST['pName'], $_POST['state'],$_POST['city'],$_POST['pid'])){
+          echo '<div class="alert alert-success" role="alert">Production Company Edited.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
+    case "Delete":
+      if (deleteProds($_POST['pid'])){
+          echo '<div class="alert alert-success" role="alert">Production Company Deleted.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
+  }
+}
+
 $prods=selectProds();
 include "view-prods.php";
 include "view-footer.php";
