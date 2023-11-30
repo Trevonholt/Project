3dd649faa41f1e564d3ce27f48555b37d634d39a
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 16:28:44 -0600

    Update model-movies.php

--- model-movies.php
+++ model-movies.php
@@ -12,4 +12,46 @@ function selectMovies() {
         throw $e;
     }
 }
+
+function insertMovies($prod_id, $genre_id, $director_id, $actor_id, $title, $rated) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `movies` (`prod_id`, `genre_id`, `director_id`, `actor_id`, `title`,`rated`) VALUES (?, ?, ?,?,?,?)");
+        $stmt->bind_param("iiiiss", $prod_id, $genre_id, $director_id, $actor_id, $title, $rated);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateMovies($prod_id, $genre_id, $director_id, $actor_id, $title, $rated, $mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `movies` SET `prod_id`=?,`genre_id`=?,`director_id`=?,`actor_id`=?,`title`=?,`rated`=? WHERE movie_id=?");
+        $stmt->bind_param("iiiissi", $prod_id, $genre_id, $director_id, $actor_id, $title, $rated, $mid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteMovies($mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from movies where movie_id=?");
+        $stmt->bind_param("i", $mid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
