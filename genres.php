b47b6a925a457697e1eacbda2cbb41d4ea7a1120
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 12:52:26 -0600

    Update genres.php

--- genres.php
+++ genres.php
@@ -4,6 +4,33 @@ require_once("model-genres.php");
 
 $pageTitle="genres";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch($_POST['actionType']){
+    case "add":
+      if (insertGenres($_POST['gType'])){
+          echo '<div class="alert alert-success" role="alert">Director Added.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
+    case "edit":
+      if (updateGenres($_POST['gType'],$_POST['gid'])){
+          echo '<div class="alert alert-success" role="alert">Director Edited.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
+    case "Delete":
+      if (deleteGenres($_POST['gid'])){
+          echo '<div class="alert alert-success" role="alert">Director Deleted.</div>';
+        } else{
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+        }
+      break;
+  }
+}
+
 $genres=selectGenres();
 include "view-genres.php";
 include "view-footer.php";
