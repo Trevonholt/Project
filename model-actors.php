f24b0deea00551b9a2170dbc09868ebeb1d58abd
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 15:23:06 -0600

    Update model-actors.php

--- model-actors.php
+++ model-actors.php
@@ -12,4 +12,46 @@ function selectActors() {
         throw $e;
     }
 }
+
+function insertActors($aFname, $aLname, $aNum) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `actors` (`actor_fname`, `actor_lname`, `actor_number`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $aFname, $aLname, $aNum);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateActors($aFname, $aLname, $aNum, $aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `actors` set `actor_fname`=?, `actor_lname`=?, `actor_number`=? where actor_id=?");
+        $stmt->bind_param("sssi", $dFname, $dLname, $dNum, $did);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteActors($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from actors where actor_id=?");
+        $stmt->bind_param("i", $aid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
