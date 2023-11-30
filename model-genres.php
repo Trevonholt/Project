dd56aa43c671d3e59a6637e15012cfc162f77fc4
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 12:56:39 -0600

    Update model-genres.php

--- model-genres.php
+++ model-genres.php
@@ -12,4 +12,46 @@ function selectGenres() {
         throw $e;
     }
 }
+
+function insertGenres($gType) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `genre` (`genre_type`) VALUES (?)");
+        $stmt->bind_param("s", $gType);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateGenres($gType, $gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `genre` set `genre_type`=? where genre_id=?");
+        $stmt->bind_param("si", $gType, $gid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteGenres($gid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from genre where genre_id=?");
+        $stmt->bind_param("i", $gid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
