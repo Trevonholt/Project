9af26e8bd91809f51fba122a3cfce22e858a4b96
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 15:59:18 -0600

    Update model-prods.php

--- model-prods.php
+++ model-prods.php
@@ -12,4 +12,46 @@ function selectProds() {
         throw $e;
     }
 }
+
+function insertProds($pName, $state, $city) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `prodComp` (`prod_name`, `state`, `city`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $pName, $state, $city);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateProds($pName, $state, $city, $pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `prodComp` set `prod_name`=?, `state`=?, `city`=? where prod_id=?");
+        $stmt->bind_param("sssi", $pName, $state, $city, $pid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteDirectors($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from prodComp where prod_id=?");
+        $stmt->bind_param("i", $pid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
