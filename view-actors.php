550fe1804db7ac4055b392eebdf4d9a1e7413952
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-11-30 15:25:09 -0600

    Update view-actors.php

--- view-actors.php
+++ view-actors.php
@@ -1,3 +1,10 @@
+<div class="row">
+  <div class="col">
+  </div>
+  <div class="col-auto">
+<?php
+include "view-actors-newform.php";
+?>
 <h1>Actors</h1>
 <div class="table-responsive">
   <table class="table">
@@ -6,7 +13,9 @@
       <th>ID</th>
       <th>First Name</th>
       <th>Last Name</th>  
-      <th>Phone Number</th>  
+      <th>Phone Number</th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -18,6 +27,22 @@ while ($actor=$actors->fetch_assoc()){
     <td><?php echo $actor['actor_fname'];?></td>
     <td><?php echo $actor['actor_lname'];?></td>
     <td><?php echo $actor['actor_number'];?></td>
+    <td>
+<?php
+include "view-actors-editform.php";
+?>
+  </td>
+    <td>
+      <form method="post" action="">
+        <input type="hidden" name="aid" value="<?php echo $actor['actor_id'];?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>
+    </td>
   </tr>
   <?php
 }
