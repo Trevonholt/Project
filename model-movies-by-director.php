63462394674576350070e111888d24a388d644a3
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 04:18:42 -0600

    Update model-movies-by-director.php

--- model-movies-by-director.php
+++ model-movies-by-director.php
@@ -1,9 +1,9 @@
 <?php
-function selectMoviesByDirector($did) {
+function selectMoviesByDirector($tid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT d.director_id, director_fname,director_lname, title, rated FROM `directors` d JOIN movies m on m.director_id=d.director_id WHERE m.director_id=?");
-        $stmt->bind_param("i", $did);
+        $stmt->bind_param("i", $tid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
