bcf7d36d8bee933404566fcb1bbe33ec2a8f3e00
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 01:42:22 -0600

    Update prod-with-directors.php

--- prod-with-directors.php
+++ prod-with-directors.php
@@ -4,6 +4,34 @@ require_once("model-prod-with-directors.php");
 
 $pageTitle="Production Companies with directors";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertMov($_POST['pid'], $_POST['gid'], $_POST['did'], $_POST['aid'], $_POST['title'], $_POST['rated'])) {
+        echo '<div class="alert alert-success" role="alert">Movie added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateMov($_POST['pid'], $_POST['gid'], $_POST['did'], $_POST['aid'], $_POST['title'], $_POST['rated'], $_POST['mid'])) {
+        echo '<div class="alert alert-success" role="alert">Movie edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteMov($_POST['mid'])) {
+        echo '<div class="alert alert-success" role="alert">Movie deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+
 $prods=selectProds();
 include "view-prod-with-directors.php";
 include "view-footer.php";
