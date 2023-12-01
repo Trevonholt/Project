c5c109a6d4e496c12bad2422e3a719a791b2f26b
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 04:02:35 -0600

    Create model-movies-by-director.php

---
+++ model-movies-by-director.php
@@ -0,0 +1,16 @@
+<?php
+function selectMoviesByDirector($did) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT d.director_id, director_fname,director_lname, title, rated FROM `directors` d JOIN movies m on m.director_id=d.director_id WHERE m.director_id=?");
+        $stmt->bind_param("i", $did);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
