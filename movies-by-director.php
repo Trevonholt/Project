78c9f36b057538f0780c6aad269983d119d329de
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 04:03:04 -0600

    Update movies-by-director.php

--- movies-by-director.php
+++ movies-by-director.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-movies-by-course.php");
+require_once("model-movies-by-director.php");
 
 $pageTitle="Movies by Director";
 include "view-header.php";
 $trevons=selectMoviesByDirector($_POST['did']);
-include "view-movies-by-course.php";
+include "view-movies-by-director.php";
 include "view-footer.php";
 ?>
