c90111bcd5d061acb73d09b94d7155ae1b67338d
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 06:49:09 -0600

    Update view-prod-with-directors-editform.php

--- view-prod-with-directors-editform.php
+++ view-prod-with-directors-editform.php
@@ -18,6 +18,7 @@
         <form method="post" action="">
           <div class="mb-3">
             <label for="pid<?php echo $director['movie_id']; ?>" class="form-label">Production Company</label>
+            <input type="text" class="form-control" id="pid<?php echo $director['movie_id']; ?>" name="pid" value="<?php echo $director['movie_id']; ?>">
 <!-- <?php
 $instructorList = selectInstructorsForInput();
 $selectedInstructor = $course['instructor_id'];
