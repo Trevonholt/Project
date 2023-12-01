3ca1128c192e0bfa3ebd4f66d430a3d02fb1cf99
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 08:27:43 -0600

    Update view-director-input-list.php

--- view-director-input-list.php
+++ view-director-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="did" name="did">
 <?php
 while ($directorItem=$directorList->fetch_assoc()){
+    $selText="";
+    if ($selectedDirector==$directorItem['director_id']){
+        $selText= " selected";
+    }
 ?>
-    <option value="<?php echo $directorItem['director_id'];?>"><?php echo $directorItem['director_fname'];?> <?php echo $directorItem['director_lname'];?></option>
+    <option value="<?php echo $directorItem['director_id'];?>"<?=$selText?>><?php echo $directorItem['director_fname'];?> <?php echo $directorItem['director_lname'];?></option>
 <?php
 }
 ?>
