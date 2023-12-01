51fb75ed00cb20a28a930f15807e5a2f4bc5f3ad
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 08:30:01 -0600

    Update view-actor-input-list.php

--- view-actor-input-list.php
+++ view-actor-input-list.php
@@ -4,6 +4,7 @@ while ($actorItem=$actorList->fetch_assoc()){
      $selText="";
     if ($selectedActor==$actorItem['actor_id']){
         $selText= " selected";
+    }
 ?>
     <option value="<?php echo $actorItem['actor_id'];?>"<?=$selText?>><?php echo $actorItem['actor_fname'];?> <?php echo $actorItem['actor_lname'];?></option>
 <?php
