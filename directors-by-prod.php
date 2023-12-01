c13d90b47f02e5db5e1be7c78ec03d14baf54e06
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 03:29:17 -0600

    Update directors-by-prod.php

--- directors-by-prod.php
+++ directors-by-prod.php
@@ -4,7 +4,7 @@ require_once("model-directors-by-prod.php");
 
 $pageTitle="Directors by Production Companies";
 include "view-header.php";
-$producers=selectDirectorsByProd($_POST['pid']);
+$producers=selectDirectorsByProd($_POST['did']);
 include "view-directors-by-prod.php";
 include "view-footer.php";
 ?>
