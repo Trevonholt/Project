b265d460315e8b64dd6b2e694bfad2fec60c360d
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 05:56:02 -0600

    Update view-prod-with-directors.php

--- view-prod-with-directors.php
+++ view-prod-with-directors.php
@@ -27,7 +27,16 @@ while ($prod=$prods->fetch_assoc()){
         <?php echo $director['director_fname'];?> <?php echo $director['director_lname'];?> - <?php echo $director['title'];?>: (<?php echo $director['rated'];?>) - <?php echo $director['director_number'];?>
         </div>
       <div class="col-auto">
-
+        <form method="post" action="">
+        <input type="hidden" name="mid" value="<?php echo $director['movie_id'];?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>
+        </form>
       </div>
     </div>
       </li>
@@ -36,6 +45,7 @@ while ($prod=$prods->fetch_assoc()){
 ?>
       </ul>
       </p>
+    </div>
       <p class="card-text"><small class="text-body-secondary">State: <?php echo $prod['state'];?></small></p>
     </div>
   </div>
