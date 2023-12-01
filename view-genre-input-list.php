4002d2aa4b87d9c84459f980919327d046f555cb
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 08:26:25 -0600

    Update view-genre-input-list.php

--- view-genre-input-list.php
+++ view-genre-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="gid" name="gid">
 <?php
 while ($genreItem=$genreList->fetch_assoc()){
+    $selText="";
+    if ($selectedGenre==$genreItem['genre_id']){
+        $selText= " selected";
+    }
 ?>
-    <option value="<?php echo $genreItem['genre_id'];?>"><?php echo $genreItem['genre_type'];?></option>
+    <option value="<?php echo $genreItem['genre_id'];?>"<?=$selText?>><?php echo $genreItem['genre_type'];?></option>
 <?php
 }
 ?>
