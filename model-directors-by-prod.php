22795a6811e3c19b816b0343f965b1340422ef5b
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 03:31:44 -0600

    Update model-directors-by-prod.php

--- model-directors-by-prod.php
+++ model-directors-by-prod.php
@@ -1,9 +1,9 @@
 <?php
-function selectDirectorsByProd($did) {
+function selectDirectorsByProd($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT d.director_id,director_fname,director_lname,director_number,prod_name FROM `directors` d JOIN movies m on m.director_id=d.director_id JOIN prodComp p on m.prod_id=p.prod_id WHERE m.prod_id=?");
-        $stmt->bind_param("i", $did);
+        $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
