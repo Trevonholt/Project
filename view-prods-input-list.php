49d5a92b3c1895077d6e2cd47a507ee3d75e562c
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-01 08:18:34 -0600

    Update view-prods-input-list.php

--- view-prods-input-list.php
+++ view-prods-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="pid" name="pid">
 <?php
 while ($prodItem=$prodlist->fetch_assoc()){
+    $selText="";
+    if ($selectedProd==$prodItem['prod_id']){
+        $selText= " selected";
+    }
 ?>
-    <option value="<?php echo $prodItem['prod_id'];?>"><?php echo $prodItem['prod_name'];?></option>
+    <option value="<?php echo $prodItem['prod_id'];?>"<?=$selText?>><?php echo $prodItem['prod_name'];?></option>
 <?php
 }
 ?>
