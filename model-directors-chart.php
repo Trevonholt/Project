cad8d86a3ab7c59e9bfe0f4e6853f0c5d4f49320
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-03 23:24:00 -0600

    Update model-directors-chart.php

--- model-directors-chart.php
+++ model-directors-chart.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectDirectors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT d.director_fname, d.director_lname, COUNT(m.movie_id) as num_movies FROM `directors` d JOIN movies m on m.director_id=d.director_id group by d.director_id ");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
