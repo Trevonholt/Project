dd1fe4ff47e2337d5b17856a3a32cae716e8669b
Trevonholt <111667957+Trevonholt@users.noreply.github.com>
2023-12-03 23:49:49 -0600

    Update view-directors-chart.php

--- view-directors-chart.php
+++ view-directors-chart.php
@@ -1,7 +1,41 @@
 <h1>Directors Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [
 <?php
 while ($director=$directors->fetch_assoc()){
+  echo $director ['director_fname','director_lname'] . ",";
 }
 ?>
-
+      ],
+      datasets: [{
+        label: '# of Movies Directed',
+        data: [
+<?php
+$directors=selectDirectors();
+while ($director=$directors->fetch_assoc()){
+  echo $director ['num_movies'] . ", ";
+}
+?>
+],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
